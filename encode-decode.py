9c9a99cae7cf8214272f3ad9d2b51bc36bd30fb3
JurneeTipton <60444944+JurneeTipton@users.noreply.github.com>
2020-03-10 23:17:51 -0400

    Update encode-decode.py

--- encode-decode.py
+++ encode-decode.py
@@ -168,3 +168,84 @@ def encode():
     newimg.save(new_img_name, str(new_img_name.split(".")[1].upper())) 
 
   
+# Decode the data in the image 
+
+def decode(): 
+
+    img = input("Enter image name(with extension) :") 
+
+    image = Image.open(img, 'r') 
+
+      
+
+    data = '' 
+
+    imgdata = iter(image.getdata()) 
+
+      
+
+    while (True): 
+
+        pixels = [value for value in imgdata.__next__()[:3] +
+
+                                  imgdata.__next__()[:3] +
+
+                                  imgdata.__next__()[:3]] 
+
+        # string of binary data 
+
+        binstr = '' 
+
+          
+
+        for i in pixels[:8]: 
+
+            if (i % 2 == 0): 
+
+                binstr += '0'
+
+            else: 
+
+                binstr += '1'
+
+                  
+
+        data += chr(int(binstr, 2)) 
+
+        if (pixels[-1] % 2 != 0): 
+
+            return data 
+
+              
+# Main Function         
+
+def main(): 
+
+    a = int(input(":: Welcome to Steganography ::\n"
+
+                        "1. Encode\n 2. Decode\n")) 
+
+    if (a == 1): 
+
+        encode() 
+
+          
+
+    elif (a == 2): 
+
+        print("Decoded word- " + decode()) 
+
+    else: 
+
+        raise Exception("Enter correct input") 
+
+          
+# Driver Code 
+
+if __name__ == '__main__' : 
+
+      
+
+    # Calling main function 
+
+    main()
