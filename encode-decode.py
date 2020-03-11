4494e67950dbd3ab7bf1e7e3405535eb2e4bf385
JurneeTipton <60444944+JurneeTipton@users.noreply.github.com>
2020-03-10 23:09:47 -0400

    Update encode-decode.py

--- encode-decode.py
+++ encode-decode.py
@@ -139,3 +139,32 @@ def encode_enc(newimg, data):
             x += 1
 
               
+# Encode data into image 
+
+def encode(): 
+
+    img = input("Enter image name(with extension): ") 
+
+    image = Image.open(img, 'r') 
+
+      
+
+    data = input("Enter data to be encoded : ") 
+
+    if (len(data) == 0): 
+
+        raise ValueError('Data is empty') 
+
+          
+
+    newimg = image.copy() 
+
+    encode_enc(newimg, data) 
+
+      
+
+    new_img_name = input("Enter the name of new image(with extension): ") 
+
+    newimg.save(new_img_name, str(new_img_name.split(".")[1].upper())) 
+
+  
